cf533a21530b2740cc6a1faac7d097e5cf2252ec
jamescfli <jamescfli@yahoo.com>
2016-12-18 17:49:25 +0800

    finished error visualization in CDF function and found misunderstanding on loss mse in training

--- relocation_office/g_finetune_topconvfc_layers.py
+++ relocation_office/g_finetune_topconvfc_layers.py
@@ -19,8 +19,7 @@ def build_vggrrfc_model(nb_fc_hidden_node=2048,
                         dropout_ratio=0.5,
                         nb_frozen_layer=0,
                         global_learning_rate=1e-5,
-                        learning_rate_multiplier=1.0,
-                        label_scaling_factor=1):
+                        learning_rate_multiplier=1.0):
     img_size = (3, img_height, img_width)
     input_tensor = Input(batch_shape=(None,) + img_size)
 
@@ -56,85 +55,84 @@ def build_vggrrfc_model(nb_fc_hidden_node=2048,
                         metrics=['mean_squared_error'])
     return vggrr_model
 
+if __name__ == '__main__':
+    # build model from scratch
+    nb_hidden_node = 2048
+    do_ratio = 0.5
+    nb_fzlayer = 11         # 11 block4, 15 block5, 19 top fc
+    learning_rate = 1e-5    # to conv layers
+    lr_multiplier = 10.0   # to top fc layers
+    label_scalar = 100      # expend from [0, 1]
+    model_stacked = build_vggrrfc_model(nb_fc_hidden_node=nb_hidden_node,
+                                        dropout_ratio=do_ratio,
+                                        nb_frozen_layer=nb_fzlayer,
+                                        global_learning_rate=learning_rate,
+                                        learning_rate_multiplier=lr_multiplier)
 
-# build model from scratch
-nb_hidden_node = 2048
-do_ratio = 0.5
-nb_fzlayer = 11         # 11 block4, 15 block5, 19 top fc
-learning_rate = 1e-5    # to conv layers
-lr_multiplier = 10.0   # to top fc layers
-label_scalar = 100      # expend from [0, 1]
-model_stacked = build_vggrrfc_model(nb_fc_hidden_node=nb_hidden_node,
-                                    dropout_ratio=do_ratio,
-                                    nb_frozen_layer=nb_fzlayer,
-                                    global_learning_rate=learning_rate,
-                                    learning_rate_multiplier=lr_multiplier,
-                                    label_scaling_factor=label_scalar)
+    batch_size = 16
+    nb_epoch = 30
+    # prepare training data
+    nb_train_sample = 13182
 
-batch_size = 16
-nb_epoch = 30
-# prepare training data
-nb_train_sample = 13182
+    datagen_train = ImageDataGenerator(rescale=1./255)
+    generator_train = datagen_train.flow_from_directory('datasets/train_test_split_480x1920_20161125/train/train_subdir/',
+                                                        target_size=(img_height, img_width),
+                                                        batch_size=batch_size,
+                                                        shuffle=True,
+                                                        class_mode='xy_pos',
+                                                        label_file="../../train_label_x{}.csv".format(label_scalar))
 
-datagen_train = ImageDataGenerator(rescale=1./255)
-generator_train = datagen_train.flow_from_directory('datasets/train_test_split_480x1920_20161125/train/train_subdir/',
-                                                    target_size=(img_height, img_width),
-                                                    batch_size=batch_size,
-                                                    shuffle=True,
-                                                    class_mode='xy_pos',
-                                                    label_file="../../train_label_x{}.csv".format(label_scalar))
+    # prepare test data
+    nb_test_sample = 2000
+    datagen_test = ImageDataGenerator(rescale=1./255)
+    # no shuffle
+    generator_test = datagen_test.flow_from_directory('datasets/train_test_split_480x1920_20161125/test/test_subdir/',
+                                                      target_size=(img_height, img_width),
+                                                      batch_size=batch_size,
+                                                      class_mode='xy_pos',
+                                                      label_file="../../test_label_x{}.csv".format(label_scalar))
 
-# prepare test data
-nb_test_sample = 2000
-datagen_test = ImageDataGenerator(rescale=1./255)
-# no shuffle
-generator_test = datagen_test.flow_from_directory('datasets/train_test_split_480x1920_20161125/test/test_subdir/',
-                                                  target_size=(img_height, img_width),
-                                                  batch_size=batch_size,
-                                                  class_mode='xy_pos',
-                                                  label_file="../../test_label_x{}.csv".format(label_scalar))
+    # fit model
+    loss_mse_rtplot = LossMseRTPlot()
+    history_callback = model_stacked.fit_generator(generator_train,
+                                                   samples_per_epoch=nb_train_sample,
+                                                   nb_epoch=nb_epoch,
+                                                   validation_data=generator_test,
+                                                   nb_val_samples=nb_test_sample,
+                                                   # callbacks=[])
+                                                   callbacks=[loss_mse_rtplot],
+                                                   verbose=1)
+    model_stacked.summary()
 
-# fit model
-loss_mse_rtplot = LossMseRTPlot()
-history_callback = model_stacked.fit_generator(generator_train,
-                                               samples_per_epoch=nb_train_sample,
-                                               nb_epoch=nb_epoch,
-                                               validation_data=generator_test,
-                                               nb_val_samples=nb_test_sample,
-                                               # callbacks=[])
-                                               callbacks=[loss_mse_rtplot],
-                                               verbose=1)
-model_stacked.summary()
+    # record
+    record = np.column_stack((np.array(history_callback.epoch) + 1,
+                              history_callback.history['loss'],
+                              history_callback.history['val_loss'],
+                              history_callback.history['mean_squared_error'],
+                              history_callback.history['val_mean_squared_error']))
 
-# record
-record = np.column_stack((np.array(history_callback.epoch) + 1,
-                          history_callback.history['loss'],
-                          history_callback.history['val_loss'],
-                          history_callback.history['mean_squared_error'],
-                          history_callback.history['val_mean_squared_error']))
-
-np.savetxt('training_procedure/convergence_vggrr2fc{}_20161125img_{}fzlayer_ls{}_{}epoch_sgdlr{}m{}_reloc_model.csv'
-           .format(nb_hidden_node,
-                   nb_fzlayer,
-                   label_scalar,
-                   nb_epoch,
-                   learning_rate,
-                   int(lr_multiplier)),
-           record, delimiter=',')
-model_stacked_json = model_stacked.to_json()
-with open('models/structure_vggrr2fc{}_20161125img_{}fzlayer_ls{}_{}epoch_sgdlr{}m{}_reloc_model.h5'
-                  .format(nb_hidden_node,
-                          nb_fzlayer,
-                          label_scalar,
-                          nb_epoch,
-                          learning_rate,
-                          int(lr_multiplier)), "w") \
-        as json_file_model_stacked:
-    json_file_model_stacked.write(model_stacked_json)
-model_stacked.save_weights('models/weights_vggrr2fc{}_20161125img_{}fzlayer_ls{}_{}epoch_sgdlr{}m{}_reloc_model.h5'
-                           .format(nb_hidden_node,
-                                   nb_fzlayer,
-                                   label_scalar,
-                                   nb_epoch,
-                                   learning_rate,
-                                   int(lr_multiplier)), overwrite=False)
+    np.savetxt('training_procedure/convergence_vggrr2fc{}_20161125img_{}fzlayer_ls{}_{}epoch_sgdlr{}m{}_reloc_model.csv'
+               .format(nb_hidden_node,
+                       nb_fzlayer,
+                       label_scalar,
+                       nb_epoch,
+                       learning_rate,
+                       int(lr_multiplier)),
+               record, delimiter=',')
+    model_stacked_json = model_stacked.to_json()
+    with open('models/structure_vggrr2fc{}_20161125img_{}fzlayer_ls{}_{}epoch_sgdlr{}m{}_reloc_model.h5'
+                      .format(nb_hidden_node,
+                              nb_fzlayer,
+                              label_scalar,
+                              nb_epoch,
+                              learning_rate,
+                              int(lr_multiplier)), "w") \
+            as json_file_model_stacked:
+        json_file_model_stacked.write(model_stacked_json)
+    model_stacked.save_weights('models/weights_vggrr2fc{}_20161125img_{}fzlayer_ls{}_{}epoch_sgdlr{}m{}_reloc_model.h5'
+                               .format(nb_hidden_node,
+                                       nb_fzlayer,
+                                       label_scalar,
+                                       nb_epoch,
+                                       learning_rate,
+                                       int(lr_multiplier)), overwrite=False)
--- relocation_office/h_visualize_error_in_cdf.py
+++ relocation_office/h_visualize_error_in_cdf.py
@@ -1,73 +1,83 @@
 __author__ = 'bsl'
 
 from utils.custom_image import ImageDataGenerator
-from relocation_office.a_finetune_topconvfc_layers import build_vggrrfc_model
+from relocation_office.g_finetune_topconvfc_layers import build_vggrrfc_model
 
 import numpy as np
 import statsmodels.api as sm # recommended import according to the docs
 import matplotlib.pyplot as plt
 
-# derive x-y values for both training and testing set
-nb_hidden_node = 2048
-do_ratio = 0.5
-nb_fzlayer = 11         # 11 block4, 15 block5, 19 top fc
-learning_rate = 1e-5    # to conv layers
-lr_multiplier = 10.0   # to top fc layers
-label_scalar = 100      # expend from [0, 1]
-model_stacked = build_vggrrfc_model(nb_fc_hidden_node=nb_hidden_node,
-                                    dropout_ratio=do_ratio,
-                                    nb_frozen_layer=nb_fzlayer,
-                                    global_learning_rate=learning_rate,
-                                    learning_rate_multiplier=lr_multiplier,
-                                    label_scaling_factor=label_scalar)
-model_stacked.load_weights('models/train_vggrr2fc2048_20161125img_11fzlayer_ls100_50epoch_sgdlr1e-5m1_reloc_model.h5')
-model_stacked.summary()
+if __name__ == '__main__':
+    # derive x-y values for both training and testing set
+    nb_hidden_node = 2048
+    do_ratio = 0.5
+    nb_fzlayer = 24         # all un-trainable layers
+    label_scalar = 100      # expend from [0, 1]
+    model_stacked = build_vggrrfc_model(nb_fc_hidden_node=nb_hidden_node,
+                                        dropout_ratio=do_ratio,
+                                        nb_frozen_layer=nb_fzlayer)
+    model_path = 'models/'
+    weight_filename = 'weights_vggrr2fc2048_20161125img_11fzlayer_ls100_50epoch_sgdlr1e-5m1_reloc_model.h5'
+    model_stacked.load_weights(model_path+weight_filename)
+    model_stacked.summary()
 
-img_height = 448
-img_width = img_height*4
-batch_size = 16
-nb_train_sample = 13182
-datagen_train = ImageDataGenerator(rescale=1./255)
-generator_train = datagen_train.flow_from_directory('datasets/train_test_split_480x1920_20161125/train/train_subdir/',
-                                                    target_size=(img_height, img_width),
-                                                    batch_size=batch_size,
-                                                    shuffle=False,
-                                                    class_mode='xy_pos',
-                                                    label_file="../../train_label_x{}.csv".format(label_scalar))
-# prepare test data
-nb_test_sample = 2000
-datagen_test = ImageDataGenerator(rescale=1./255)
-# no shuffle
-generator_test = datagen_test.flow_from_directory('datasets/train_test_split_480x1920_20161125/test/test_subdir/',
-                                                  target_size=(img_height, img_width),
-                                                  batch_size=batch_size,
-                                                  shuffle=False,
-                                                  class_mode='xy_pos',
-                                                  label_file="../../test_label_x{}.csv".format(label_scalar))
-train_pos = model_stacked.predict_generator(generator_train)
-test_pos = model_stacked.predict_generator(generator_test)
-# check
-print "train pos xy shape: {}".format(train_pos.shape)
-print "test pos xy shape: {}".format(test_pos.shape)
+    img_height = 448
+    img_width = img_height*4
+    batch_size = 2
+    nb_train_sample = 13182
+    datagen_train = ImageDataGenerator(rescale=1./255)
+    generator_train = datagen_train.flow_from_directory('datasets/train_test_split_480x1920_20161125/train/train_subdir/',
+                                                        target_size=(img_height, img_width),
+                                                        batch_size=batch_size,
+                                                        shuffle=False,
+                                                        class_mode='xy_pos',
+                                                        label_file="../../train_label_x{}.csv".format(label_scalar))
+    nb_test_sample = 2000
+    datagen_test = ImageDataGenerator(rescale=1./255)
+    generator_test = datagen_test.flow_from_directory('datasets/train_test_split_480x1920_20161125/test/test_subdir/',
+                                                      target_size=(img_height, img_width),
+                                                      batch_size=batch_size,
+                                                      shuffle=False,
+                                                      class_mode='xy_pos',
+                                                      label_file="../../test_label_x{}.csv".format(label_scalar))
+    train_pos = model_stacked.predict_generator(generator_train,
+                                                val_samples=nb_train_sample)
+    test_pos = model_stacked.predict_generator(generator_test,
+                                               val_samples=nb_test_sample)
+    # check
+    print "train pos xy shape: {}".format(train_pos.shape)
+    print "test pos xy shape: {}".format(test_pos.shape)
+    np.save(open('predicted_data/train_position_result.npy', 'w'), train_pos)
+    np.save(open('predicted_data/test_position_result.npy', 'w'), test_pos)
 
-# derive L2 error for both sets
-train_error = np.linalg.norm(train_pos, axis=1)     # if axis=0 is the sample index
-test_error = np.linalg.norm(test_pos, axis=1)
-train_error = train_error*3600/label_scalar
-test_error = test_error*3600/label_scalar
+    # load the xy pos
+    train_pos = np.load(open('predicted_data/train_position_result.npy', 'r'))
+    test_pos = np.load(open('predicted_data/test_position_result.npy', 'r'))
+    train_pos_gt = np.loadtxt('datasets/train_test_split_480x1920_20161125/train_label_x{}.csv'
+                              .format(label_scalar), dtype='float32', delimiter=',')
+    test_pos_gt = np.loadtxt('datasets/train_test_split_480x1920_20161125/test_label_x{}.csv'
+                             .format(label_scalar), dtype='float32', delimiter=',')
+    # derive L2 error for both sets
+    train_error = np.linalg.norm(train_pos - train_pos_gt, ord=2, axis=1)     # if axis=0 is the sample index
+    test_error = np.linalg.norm(test_pos - test_pos_gt, ord=2, axis=1)
+    train_loss_mse = (train_error**2).mean()    # 0.56459689 vs 5.1683 in keras 50epoch
+    test_loss_mse = (test_error**2).mean()      # 0.62018335 vs 0.3101 in keras 50epoch
+    train_error = train_error*3600/label_scalar     # resume to cm
+    test_error = test_error*3600/label_scalar
 
-# draw pdf and cdf of the error in both sets
-ecdf_train = sm.distributions.ECDF(train_error)
-ecdf_test = sm.distributions.ECDF(test_error)
+    # draw pdf and cdf of the error in both sets
+    ecdf_train = sm.distributions.ECDF(train_error)
+    ecdf_test = sm.distributions.ECDF(test_error)
 
-x_train = np.linspace(0, max(train_error), 500)
-x_test = np.linspace(0, max(test_error), 500)
-# pdf =
-cdf_train = ecdf_train(x_train)
-cdf_test = ecdf_test(x_test)
-plt.step(x_train, cdf_train)
-plt.step(x_test, cdf_test)
-plt.xlabel("Error (cm)")
-plt.ylabel('CDF')
-plt.legend(['Train', 'Test'])
-plt.show()
+    x_train = np.linspace(0, max(train_error), 500)
+    x_test = np.linspace(0, max(test_error), 500)
+    # pdf =
+    cdf_train = ecdf_train(x_train)
+    cdf_test = ecdf_test(x_test)
+    plt.step(x_train, cdf_train)
+    plt.step(x_test, cdf_test)
+    plt.xlim([0, max([max(train_error), max(test_error)])])
+    plt.xlabel("Error (cm)")
+    plt.ylabel('CDF')
+    plt.legend(['Train', 'Test'])
+    plt.show()
